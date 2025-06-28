3a35184daa1435a43cd4148aabe8bdf69674bab0
DamianMNXOnline <damian@mnxonline.com>
2025-06-28 10:39:41 +0100

    feat: create + get users

--- apps/jobber-auth/src/users/users.resolver.ts
+++ apps/jobber-auth/src/users/users.resolver.ts
@@ -1,11 +1,19 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Mutation, Query, Resolver, Args } from '@nestjs/graphql';
 import { User } from './models/user.model';
 import { UsersService } from './users.service';
+import { CreateUserInput } from './dto/create-user.input';
 
 @Resolver(() => User)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
+  @Mutation(() => User)
+  async createUser(@Args('createUserInput') createUserInput: CreateUserInput) {
+    return this.usersService.createUser(createUserInput);
+  }
+
   @Query(() => [User], { name: 'users' })
-  async getUsers() {}
+  async getUsers() {
+    return this.usersService.getUsers();
+  }
 }
--- apps/jobber-auth/src/users/users.service.ts
+++ apps/jobber-auth/src/users/users.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from 'prisma-clients/jobber-auth';
+import { PrismaService } from '../app/prisma/prisma.service';
+import { hash } from 'bcryptjs';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async createUser(data: Prisma.UserCreateInput) {
+    const user = await this.prismaService.user.create({
+      data: {
+        ...data,
+        password: await hash(data.password, 10),
+      },
+    });
+    return user;
+  }
+
+  async getUsers() {
+    return this.prismaService.user.findMany();
+  }
+}
